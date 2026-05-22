73c82f1cd1e3a384e9a5d885724295594c4089df
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2026-05-23 01:57:02 +0530

    Make chnages for mobile

--- src/pages/RedirectedPage.jsx
+++ src/pages/RedirectedPage.jsx
@@ -13,11 +13,9 @@ function RedirectedPage() {
 
       alert("Review copied successfully!");
 
-      window.open(
-        "https://www.google.com/search?sca_esv=3e027c27d603e6ff&sxsrf=ANbL-n5uYtnhDHkBpx3JiLvBZhq4U1t31g:1779469100829&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOVLe8BRxjm4WywqacjjKWtArD9-3JAvaLBp1X8RwPrWH1Kc2DFHEseXqY37itOj7vpkwnm3zHF3narWVFrQPIYvgyBel&q=HOTEL+VEDAS+Reviews&sa=X&ved=2ahUKEwj_75L9rs2UAxULxzgGHWBOLDQQ0bkNegQIJBAF&biw=1440&bih=900&dpr=2#lrd=0x3bdd0160494fdf1b:0x8ef88c08566368c2,3,,,,",
-
-        "_blank"
-      );
+      window.location.href =
+        ("https://www.google.com/search?sca_esv=3e027c27d603e6ff&sxsrf=ANbL-n5uYtnhDHkBpx3JiLvBZhq4U1t31g:1779469100829&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOVLe8BRxjm4WywqacjjKWtArD9-3JAvaLBp1X8RwPrWH1Kc2DFHEseXqY37itOj7vpkwnm3zHF3narWVFrQPIYvgyBel&q=HOTEL+VEDAS+Reviews&sa=X&ved=2ahUKEwj_75L9rs2UAxULxzgGHWBOLDQQ0bkNegQIJBAF&biw=1440&bih=900&dpr=2#lrd=0x3bdd0160494fdf1b:0x8ef88c08566368c2,3,,,,",
+        "_blank");
     } catch (error) {
       console.log(error);
 
